ff775f6c5d6af2fa077d87526dbc0d7974166ef5
orelshalom <orelshalom1@gmail.com>
2020-01-07 14:55:42 +0200

    main

--- src/actions/Main.java
+++ src/actions/Main.java
@@ -31,15 +31,15 @@ public class Main {
 		wf.write();
 		Filter f1 = new IDFilter("PKQ1.1302.001");
 		f1.filter(scs);
-//		EarthCoordinate ec = new EarthCoordinate(34.95398122, 32.08680173, 82.4484252929687);
-//		Filter f2 = new LocationFilter(ec, 35.0);
-//		f2.filter(scs);
-//	    SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		GregorianCalendar gc1 = new GregorianCalendar(), gc2 = new GregorianCalendar();
-//		gc1.setTime(fmt.parse("2019-12-06 16:18:12"));
-//		gc2.setTime(fmt.parse("2019-12-08 08:30:09"));
-//		Filter f3 = new TimeFilter(gc1, gc2);
-//		f3.filter(scs);
+		EarthCoordinate ec = new EarthCoordinate(34.95398122, 32.08680173, 82.4484252929687);
+		Filter f2 = new LocationFilter(ec, 35.0);
+		f2.filter(scs);
+	    SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		GregorianCalendar gc1 = new GregorianCalendar(), gc2 = new GregorianCalendar();
+		gc1.setTime(fmt.parse("2019-12-06 16:18:12"));
+		gc2.setTime(fmt.parse("2019-12-08 08:30:09"));
+		Filter f3 = new TimeFilter(gc1, gc2);
+		f3.filter(scs);
 	}
 
 }
